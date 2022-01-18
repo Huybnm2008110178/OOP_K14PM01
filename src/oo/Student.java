0c0997d2d71fed0da895fab460b2ec9e68ab69ac
Bui Nhat Minh Huy <minhhuybui559@gmail.com>
2022-01-18 14:14:32 +0700

    tuan 3

---
+++ src/oo/Student.java
@@ -0,0 +1,30 @@
+package oo;
+
+public class Student {
+    
+    //attribute - data - state 
+    boolean gioiTinh;
+    float tuoi;
+    String name;
+    String khoa;
+    double diemTB;
+
+    //method - functions - behavior
+
+    void hoc(){
+        System.out.println("Hoc ...");
+    
+    }
+
+    void thi(){
+        System.out.println("Thi ...");
+
+    }
+
+    void dangkyMonHoc(){
+        System.out.println();
+   
+    }
+
+    
+}
---
+++ src/oo/StudentTestDrive.java
@@ -0,0 +1,17 @@
+package oo;
+
+public class StudentTestDrive {
+    public static void main(String[] args){
+        
+        Student tuanStudent = new Student();
+        tuanStudent.hoc();
+
+        Student ducStudent = new Student();
+        ducStudent.thi();
+
+        Student xyzStudent = new Student();
+        xyzStudent.dangkyMonHoc();
+        
+    }
+    
+}
