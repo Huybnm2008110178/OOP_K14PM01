46df6ccf05f472a9a79d726adcfb0eb8826c587b
Bui Nhat Minh Huy <Huybnm2008110178>
2022-03-24 16:01:50 +0700

    update

---
+++ src/oo/tuan6/buoi9/TestMatHanhVi.java
@@ -0,0 +1,12 @@
+package oo.tuan6.buoi9;
+
+import oo.tuan3.buoi5.Dog;
+import oo.tuan5.buoi9.Animal;
+
+public class TestMatHanhVi {
+    public static void main(String[] args) {
+        Animal animal = new Dog();
+        Dog dog = (Dog) animal;//ep kieu
+    }
+    
+}
